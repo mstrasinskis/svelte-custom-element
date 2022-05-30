707c953e11d299b7264077b3523051dc0d4ac260
mstrasinskis <maksims.strasinskis@dfinity.org>
2022-05-30 17:55:34 +0200

    feat: add customElement "my-button"

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import App from './App.svelte';
+// import App from "./App.svelte";
+import Button from "./Button.svelte";
 
-const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
-});
+// const app = new App({
+//   target: document.body,
+//   props: {
+//     name: "world",
+//   },
+// });
 
-export default app;
+// export default app;
